c898a8e181f08274426a4a370277c21dbc7800f8
Apoorv Vikram Singh Rathore <90571448+Apoorv-vikram-singh@users.noreply.github.com>
2023-06-19 20:19:55 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (55.98%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,28 +1,22 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int nr = image.length;
-        int nc = image[0].length;
-        //Error Checks
-        //If the starting pixel is already colored as 'colar',no changes are made to the image.
-        if(image == null || image.length == 0 || image[sr][sc] == color)
-            return image;
-        //image, sr, sc, startColor, fillColor, no. of rows, no. of Cols
-        return dfs(image, sr, sc,image[sr][sc],color, nr, nc);
-    }
-    private int[][] dfs(int[][] image, int sr, int sc, int startColor,
-                        int fillColor,int nr, int nc) {
-        //Check for boundaries and current pixel color not equal start pixel color
-        if (sr < 0 || sc < 0 || sr >= nr || sc >= nc || image[sr][sc] != startColor) {
-          return image;
-        }
-        //Fill the pixel with new value 
-        image[sr][sc] = fillColor;
-        //call recursively for 4 co-ordinates
-        dfs(image, sr - 1, sc,startColor, fillColor, nr, nc);
-        dfs(image, sr + 1, sc,startColor, fillColor, nr, nc);
-        dfs(image, sr, sc - 1,startColor, fillColor, nr, nc);
-        dfs(image, sr, sc + 1,startColor, fillColor, nr, nc);
+        if(image[sr][sc]==color)return image;
+        
+        DFS(image,sr,sc,color,image[sr][sc]);
         return image;
+    }
+    void DFS(int[][] grid,int sr,int sc,int color,int old)
+    {
+        if(sr<0||sc<0||sr>=grid.length||sc>=grid[0].length||grid[sr][sc]!=old)
+        {
+            return;
+        }
+        
+        grid[sr][sc]=color;
+        DFS(grid,sr+1,sc,color,old);
+        DFS(grid,sr-1,sc,color,old);
+        DFS(grid,sr,sc+1,color,old);
+        DFS(grid,sr,sc-1,color,old);
         
     }
 }
